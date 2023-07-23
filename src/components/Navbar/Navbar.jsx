ab7af5b225cb455e96eaa0d4f5fb11f3a8076962
KKDEVE <116938392+Codedkk@users.noreply.github.com>
2023-07-24 00:51:36 +0800

    file structure has been built

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+    const [navOpen, setNavOpen] = useState(false)
     return (
-        <div className="navbar">Navbar</div>
+        <div className="navbar">
+            <div className="nav_left">
+                <Link to="/" className="logo link">lKKlGREEN</Link>
+            </div>
+            <div className="nav_right">
+                {/* <div className="nav_right-m"></div> */}
+                <div className="nav_right-xxs">
+                    {!navOpen ? <i className="fa-solid fa-ellipsis" onClick={() => setNavOpen(!navOpen)}></i> :
+                        <i className="fas fa-times" onClick={() => setNavOpen(!navOpen)}></i>}
+
+                    {navOpen && <div className="ham-menu">
+                        <button className="button button_resume">GET MY RESUME</button>
+                        <button className="button button_hi">SAY HI</button>
+                    </div>}
+                </div>
+            </div>
+
+        </div>
     )
 }
 
