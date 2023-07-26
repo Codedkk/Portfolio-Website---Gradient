f4b0053ffafcb7189975c76aa3eb7d79230d3d47
KKDEVE <116938392+Codedkk@users.noreply.github.com>
2023-07-27 01:01:18 +0800

    hamburger menu built

---
+++ src/components/Navbar/Menu.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Menu = () => {
+    const [navOpen, setNavOpen] = useState(false)
+    return (
+        <div className="menu">
+            <i className="fa-solid fa-ellipsis " onClick={() => setNavOpen(!navOpen)}></i>
+            {navOpen &&
+                <div className="ham-menu" >
+                    <i className="fas fa-times ham-menu-close" onClick={() => setNavOpen(!navOpen)}></i>
+                    <Link to="/project" className="link">PROJECTS</Link>
+                    <Link to="/project" className="link">RESUME</Link>
+                    <Link to="/project" className="link">ABOUT</Link>
+                    <button className="button button-ham ">✧ Say HI! ✧</button>
+                </div>}
+
+        </div>
+    )
+
+}
+export default Menu
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,20 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Menu from './Menu';
 
 const Navbar = () => {
-    const [navOpen, setNavOpen] = useState(false)
     return (
         <nav className="navbar container">
             <div className="nav_left">
                 <Link to="/" className="logo link">KOliGREEN</Link>
             </div>
+            <Menu />
             <div className="nav_right">
-                <div className="nav_right-s" >
+                <div className="navbar-menu">
                     <Link to="/project" className="link">PROJECTS</Link>
                     <Link to="/project" className="link">RESUME</Link>
                     <Link to="/project" className="link">ABOUT</Link>
                     <button className="button button-nav">✧ Say HI! ✧</button>
-                </div>
-
-                <div className="nav_right-xxs">
-                    <i className="fa-solid fa-ellipsis" onClick={() => setNavOpen(!navOpen)}></i>
-                    {navOpen && <div className="ham-menu">
-                        <i className="fas fa-times ham-menu-close" onClick={() => setNavOpen(!navOpen)}></i>
-                        <button className="button button_resume">GET MY RESUME</button>
-                        <button className="button button_hi">SAY HI</button>
-                    </div>}
                 </div>
             </div>
 
