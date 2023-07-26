f881a0b6f71bae55eeb579b2bb39e5de7ec814c7
KKDEVE <116938392+Codedkk@users.noreply.github.com>
2023-07-27 05:52:15 +0800

    hero-responsive: built

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,10 +4,37 @@ const Hero = () => {
     return (
         <div className="hero">
             <div className="hero_text">
-                <div className="hero_text-first">Koli is a</div>
-                <div className="hero_text-second">✷︎ marketing strategist</div>
-                <div className="hero_text-third">with ✹empathyn and</div>
-                <div className="hero_text-fourth">✶visual lorem</div>
+                <span >Fili is &nbsp;
+                    <span>a&nbsp;
+                        <div className="hero_text-first">
+                            <span> ✷︎ </span>
+                            <span>creative developer,</span>
+                        </div>&nbsp;</span>
+                    <span>
+                        <div className="hero_text-second">
+                            <span >✎︎ </span>
+                            <span> fusing usability with <span className="hero_text-style">style</span></span>
+                        </div>&nbsp;</span>
+                    {/* <span>with
+                        <div className="hero_text-third">
+                            <span>✹︎ </span>
+                            <span >visual lorem</span>
+                        </div>&nbsp;
+                    </span> */}
+
+                </span>
+                {/* <span className="hero_text-first">
+                    <span>Koli is a</span>&nbsp;
+
+
+                    <span className="hero_text-second">
+                        <span><span>✷︎ </span><span>marketing strategist</span></span>&nbsp;</span>
+
+                    <span className="hero_text-third"><span>✹</span><span><span>empathyn</span> and</span>&nbsp;
+                    </span>
+                    <span className="hero_text-fourth"><span>✶</span><span>visual lorem</span>&nbsp;</span>
+                </span> */}
+
             </div>
             <button className="button button-hero">✧ Say HI! ✧</button>
             <div className="hero_social">
