758dc46682901af5e809595268e80aa779f88d8c
KKDEVE <116938392+Codedkk@users.noreply.github.com>
2023-07-31 02:03:08 +0800

    Resume all screens: built

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,19 +3,21 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className="footer container">
-            <div className="footer-content">
+            {/* <div className="footer-content">
                 <div className="footer-nav">
-
+                    <div>Projects</div>
+                    <div>Resume</div>
+                    <div>About</div>
+                    <div>Work with me</div>
                 </div>
                 <div className="footer-icons">
 
                 </div>
 
                 <div className="footer-by">
-
+                    Developed By <span className="logo">KIKIDENG</span>
                 </div>
-            </div>
-
+            </div> */}
         </div>
 
     )
